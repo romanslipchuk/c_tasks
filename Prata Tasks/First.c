3dbb4c121f3bc47def5aeed8b5c0600e31261df0
Roman Slipchuk <roman.slipchuk@gmail.com>
2022-10-09 12:00:49 +0300

    Add first file to test

---
+++ Prata Tasks/First.c
@@ -0,0 +1,5 @@
+//
+// Created by romasl on 09.10.22.
+//
+
+#include "First.h"
---
+++ Prata Tasks/First.h
@@ -0,0 +1,8 @@
+//
+// Created by romasl on 09.10.22.
+//
+
+#ifndef C_TASKS_FIRST_H
+#define C_TASKS_FIRST_H
+
+#endif //C_TASKS_FIRST_H
