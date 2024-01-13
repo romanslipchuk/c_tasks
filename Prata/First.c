829c1e5d1ca57ad3018aaac054bdbd55bd5037fa
Roman Slipchuk <roman.slipchuk@gmail.com>
2024-01-13 08:21:37 +0700

    remove .o files

--- Prata/First.c
+++ Prata/First.c
@@ -3,3 +3,14 @@
 //
 
 #include "First.h"
+#include <stdio.h>
+
+int main(void){
+    int num;
+    num = 1;
+
+    printf("Just a comp ");
+    printf("Simple comp\n");
+    printf("My favorite numbes is %d, just because i like it\n", num);
+    return 0;
+}
